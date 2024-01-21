d2b66cd6699355bf2998e17e1de005edf464cb28
bugdea1er <bugdealer@icloud.com>
2024-01-21 17:33:15 +0300

    Formatting

--- tests/directory_test.cpp
+++ tests/directory_test.cpp
@@ -6,40 +6,40 @@ using tmp::directory;
 namespace fs = std::filesystem;
 
 TEST(DirectoryTest, CreateDirectory) {
-  const auto tmpdir = directory("test");
-  ASSERT_TRUE(fs::exists(tmpdir));
+    const auto tmpdir = directory("test");
+    ASSERT_TRUE(fs::exists(tmpdir));
 
-  std::cout << fs::path(tmpdir) << std::endl;
+    std::cout << fs::path(tmpdir) << std::endl;
 }
 
 TEST(DirectoryTest, RemoveDirectory) {
-  fs::path path;
-  {
-    const auto tmpdir = directory("test");
-    path = tmpdir;
-    ASSERT_TRUE(fs::exists(path));
-  }
+    fs::path path;
+    {
+        const auto tmpdir = directory("test");
+        path = tmpdir;
+        ASSERT_TRUE(fs::exists(path));
+    }
 
-  EXPECT_FALSE(fs::exists(path));
+    EXPECT_FALSE(fs::exists(path));
 }
 
 TEST(DirectoryTest, CreateMultiple) {
-  const auto path = "test";
+    const auto path = "test";
 
-  const auto fst = directory(path);
-  ASSERT_TRUE(fs::exists(fst));
+    const auto fst = directory(path);
+    ASSERT_TRUE(fs::exists(fst));
 
-  const auto snd = directory(path);
-  ASSERT_TRUE(fs::exists(snd));
+    const auto snd = directory(path);
+    ASSERT_TRUE(fs::exists(snd));
 
-  EXPECT_NE(fs::path(fst), fs::path(snd));
+    EXPECT_NE(fs::path(fst), fs::path(snd));
 }
 
 TEST(DirectoryTest, SubpathTest) {
-  const auto tmpdir = directory("test");
-  const auto child = tmpdir / "child";
+    const auto tmpdir = directory("test");
+    const auto child = tmpdir / "child";
 
-  ASSERT_EQ(tmpdir, child.parent_path());
+    ASSERT_EQ(tmpdir, child.parent_path());
 }
 
 TEST(DirectoryTest, MoveConstruction) {
