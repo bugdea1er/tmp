b5df12e20edefbdc34206bb1580257d782dc2a24
bugdea1er <bugdealer@icloud.com>
2024-01-28 19:39:49 +0300

    Move the path parameter in tmp::path constructor

--- include/tmp/path.hpp
+++ include/tmp/path.hpp
@@ -52,7 +52,7 @@ protected:
     /// Creates a unique temporary path using the given constructor function.
     /// @param prefix the path between system temp
     /// @param creator wrapped mktemp-like function that returns resulting path
-    explicit path(std::filesystem::path path) : underlying(path) {}
+    explicit path(std::filesystem::path path) : underlying(std::move(path)) {}
 
     static std::filesystem::path make_parent(std::string_view prefix) {
         const auto parent = std::filesystem::temp_directory_path() / prefix;
