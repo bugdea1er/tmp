f3774c34e7ce082058a54e736836dd2f284dcf8c
Ilya Andreev <bugdealer@icloud.com>
2024-01-30 00:15:10 +0300

    Documentation (#10)
    Rewrite documentation for all classes and methods

--- include/tmp/directory.hpp
+++ include/tmp/directory.hpp
@@ -7,48 +7,50 @@
 
 namespace tmp {
 
-/// The tmp::directory is a smart handle that owns and manages a temporary
-/// directory and disposes of it when this handle goes out of scope. It
-/// simplifies the process of creating and managing temporary directories by
-/// ensuring that they are properly cleaned up when they are no longer needed.
+/// tmp::directory is a smart handle that owns and manages a temporary directory
+/// and deletes it recursively when this handle goes out of scope
 ///
 /// When a tmp::directory object is created, it creates a unique temporary
-/// directory using the system's default location for temporary files. If a
-/// prefix is provided to the constructor, the directory is created in the path
-/// <system's default location for temporary files>/prefix/. The prefix can be
-/// a path consisting of multiple segments.
+/// directory using the system's default location for temporary files; the path
+/// consists of the system's temporary directory path, the given prefix, and
+/// random characters to ensure path uniqueness
 ///
-/// When the object is destroyed, it deletes the temporary recursively directory
+/// The managed directory is deleted of when either of the following happens:
+/// - the managing tmp::directory object is destroyed
+/// - the managing tmp::directory object is assigned another path via operator=
+///
+/// The following example uses a tmp::directory object to create a temporary
+/// directory with the product identifier prefix; when the function returns,
+/// the tmp::directory object goes out of scope and the temporary directory is
+/// deleted along with all of its contents:
 ///
 /// @code{.cpp}
 ///   #include <tmp/directory.hpp>
 ///
 ///   auto func() {
-///     tmp::directory tmpdir { "org.example.product" };
-///
-///     // use the temporary directory without worrying about cleanup
+///     auto tmpdir = tmp::directory("org.example.product");
 ///
 ///     // the temporary directory is deleted recursively when the
 ///     // tmp::directory object goes out of scope and is destroyed
 ///   }
 /// @endcode
-///
-/// The above example uses a tmp::directory object to create a temporary
-/// directory with the product identifier prefix. When the function returns,
-/// the tmp::directory object goes out of scope and the temporary directory is
-/// deleted along with all of its contents.
 class directory final : public path {
 public:
-    /// Creates a unique temporary directory using the system's default location
-    /// for temporary files. If a prefix is provided to the constructor, the
-    /// directory is created in the path <temp dir>/prefix/. The prefix can be
-    /// a path consisting of multiple segments.
+    /// Creates a unique temporary directory
+    ///
+    /// The directory path consists of the system's temporary directory path,
+    /// the given prefix, and random characters to ensure path uniqueness
+    ///
+    /// @param prefix   A prefix to be used in the temporary directory path
+    /// @throws std::filesystem::filesystem_error if cannot create a directory
     explicit directory(std::string_view prefix = "");
 
     /// Concatenates this directory path with a given @p source
+    /// @param source   A string which represents a path name
+    /// @returns The result of path concatenation
     std::filesystem::path operator/(std::string_view source) const;
 
-    /// Deletes this directory recursively when the enclosing scope is exited
+    /// Deletes the managed directory recursively if its path is not empty
     ~directory() noexcept override;
 
     directory(directory&&) noexcept;               ///< move-constructible
--- include/tmp/file.hpp
+++ include/tmp/file.hpp
@@ -7,64 +7,65 @@
 
 namespace tmp {
 
-/// The tmp::file class is a smart handle that owns and manages a temporary file
-/// and disposes of it when this handle goes out of scope. It simplifies the
-/// process of creating and managing temporary files by ensuring that they are
-/// properly cleaned up when they are no longer needed.
+/// tmp::file is a smart handle that owns and manages a temporary file and
+/// deletes it when this handle goes out of scope
 ///
-/// When a tmp::file object is created, it creates a temporary file using the
-/// system's default location for temporary files. If a prefix is provided to
-/// the constructor, the file is created in the path
-/// <system's default location for temporary files>/prefix/. The prefix can be
-/// a path consisting of multiple segments.
+/// When a tmp::file object is created, it creates a unique temporary file using
+/// the system's default location for temporary files; the path consists of the
+/// system's temporary directory path, the given prefix, and random characters
+/// to ensure path uniqueness
 ///
-/// The tmp::file class also provides an additional write and append methods
-/// which allow writing data to the temporary file.
+/// The managed file is deleted of when either of the following happens:
+/// - the managing tmp::file object is destroyed
+/// - the managing tmp::file object is assigned another path via operator=
 ///
-/// When the object is destroyed, it deletes the temporary file.
+/// tmp::file provides additional `write` and `append` methods
+/// which allow writing data to the temporary file
 ///
-/// @note The tmp::file class cannot be copied or moved, as it is designed to
-/// manage a single temporary file. If you need to create multiple temporary
-/// files, you should create multiple instances of the class.
+/// The following example uses a tmp::file object to create a temporary file
+/// and write a string to it; when the function returns, the tmp::file object
+/// goes out of scope and the temporary file:
 ///
 /// @code{.cpp}
 ///   #include <tmp/file.hpp>
 ///
-///   auto prepareFile(const std::string& content) {
-///     tmp::file tmpfile { "org.example.product" };
-///     tmpfile << content;
+///   auto func(std::string_view content) {
+///     auto tmpfile = tmp::file("org.example.product");
+///     tmpfile.write(content);
 ///
-///     // use the temporary file without worrying about cleanup
-///
-///     // the temporary file is deleted when the tmp::file object goes out
-///     // of scope and is destroyed
-///  }
+///     // the temporary file is deleted recursively when the
+///     // tmp::file object goes out of scope and is destroyed
+///   }
 /// @endcode
-///
-/// The above example uses a tmp::file object to create a temporary file with
-/// the product identifier prefix. When the function returns, the tmp::file
-/// object goes out of scope and the temporary file is deleted.
 class file final : public path {
 public:
-    /// Creates a unique temporary binary file using the system's default
-    /// location for temporary files. If a prefix is provided to the
-    /// constructor, the directory is created in the path <temp dir>/prefix/.
-    /// The prefix can be a path consisting of multiple segments.
+    /// Creates a unique temporary binary file
+    ///
+    /// The file path consists of the system's temporary directory path, the
+    /// given prefix, and random characters to ensure path uniqueness
+    ///
+    /// @param prefix   A prefix to be used in the temporary file path
+    /// @throws std::filesystem::filesystem_error if cannot create a file
     explicit file(std::string_view prefix = "");
 
-    /// Creates a unique temporary text file using the system's default location
-    /// for temporary files. If a prefix is provided to the constructor, the
-    /// directory is created in the path <temp dir>/prefix/. The prefix can be
-    /// a path consisting of multiple segments.
+    /// Creates a unique temporary text file
+    ///
+    /// The file path consists of the system's temporary directory path, the
+    /// given prefix, and random characters to ensure path uniqueness
+    ///
+    /// @param prefix   A prefix to be used in the temporary file path
+    /// @throws std::filesystem::filesystem_error if cannot create a file
     static file text(std::string_view prefix = "");
 
-    /// Writes the given @p content to this file discarding any previous content
+    /// Writes the given content to this file discarding any previous content
+    /// @param content  A string to write to this file
     void write(std::string_view content) const;
 
-    /// Appends the given @p content to the end of this file
+    /// Appends the given content to the end of this file
+    /// @param content  A string to append to this file
     void append(std::string_view content) const;
 
-    /// Deletes this file when the enclosing scope is exited
+    /// Deletes the managed file if its path is not empty
     ~file() noexcept override;
 
     file(file&&) noexcept;                   ///< move-constructible
@@ -73,12 +74,17 @@ public:
     auto operator=(const file&) = delete;    ///< not copy-assignable
 
 private:
-    bool binary;    ///< This file write mode
+    /// Whether the managed file is opened in binary write mode
+    bool binary;
 
-    /// Creates a unique temporary file using the system's default location
-    /// for temporary files. If a prefix is provided to the constructor, the
-    /// directory is created in the path <temp dir>/prefix/. The prefix can be
-    /// a path consisting of multiple segments.
+    /// Creates a unique temporary file
+    ///
+    /// The file path consists of the system's temporary directory path, the
+    /// given prefix, and random characters to ensure path uniqueness
+    ///
+    /// @param prefix   A prefix to be used in the temporary file path
+    /// @param binary   Whether the managed file is opened in binary write mode
+    /// @throws std::filesystem::filesystem_error if cannot create a file
     explicit file(std::string_view prefix, bool binary);
 };
 
