4177cf8e50205915674367524dd368d5583cdbf0
bugdea1er <bugdealer@icloud.com>
2024-02-03 18:08:52 +0300

    Add tmp::path::move

--- lib/path.cpp
+++ lib/path.cpp
@@ -8,6 +8,10 @@ namespace fs = std::filesystem;
 
 namespace {
 
+/// Options for recursive overwriting coping
+const fs::copy_options copy_options = fs::copy_options::recursive
+                                    | fs::copy_options::overwrite_existing;
+
 /// Deletes the given path recursively, ignoring any errors
 /// @param path The path of the directory to remove
 void remove(const tmp::path& path) noexcept {
@@ -48,6 +52,21 @@ fs::path path::release() noexcept {
     fs::path path = std::move(underlying);
     underlying.clear();
     return path;
+}
+
+void path::move(const fs::path& to) {
+    std::error_code ec;
+    fs::rename(*this, to, ec);
+    if (ec == std::errc::cross_device_link) {
+        fs::copy(*this, to, copy_options, ec);
+    }
+
+    if (ec) {
+        throw fs::filesystem_error("Cannot move temporary resource", to, ec);
+    }
+
+    remove(*this);
+    release();
 }
 
 fs::path path::make_pattern(std::string_view prefix) {
