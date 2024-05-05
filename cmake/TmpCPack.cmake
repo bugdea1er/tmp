bf42d10613be3bf319e262b0a76b9bd1d959b897
ignat.sergeev <ignat.sergeev@softcom.su>
2024-05-05 04:31:11 +0300

    Added deb generator

---
+++ cmake/TmpCPack.cmake
@@ -0,0 +1,21 @@
+set(CPACK_PACKAGE_DIRECTORY "${CMAKE_BINARY_DIR}/_packages")
+set(CPACK_PACKAGE_VERSION ${PROJECT_VERSION})
+set(CPACK_PACKAGING_INSTALL_PREFIX /usr)
+
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Ignat Sergeev")
+set(CPACK_DEB_COMPONENT_INSTALL ON)
+
+set(CPACK_DEBIAN_TMP_PACKAGE_NAME "libtmp")
+set(CPACK_DEBIAN_TMP-DEV_PACKAGE_NAME "libtmp-dev")
+set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
+
+set(CPACK_DEBIAN_TMP_PACKAGE_DEPENDS "libstdc++6 (>= 8)")
+set(CPACK_DEBIAN_TMP-DEV_PACKAGE_DEPENDS
+	    "libstdc++-8-dev | libstdc++-9-dev | libstdc++-10-dev | libstdc++-11-dev | libstdc++-12-dev | libstdc++-13-dev | libstdc++-14-dev, libtmp (= ${CPACK_PACKAGE_VERSION})"
+)
+
+set(CPACK_DEBIAN_TMP_DESCRIPTION "RAII-wrappers for unique temporary files and directories that are deleted automatically for C++17 and later. Library package")
+set(CPACK_DEBIAN_TMP-DEV_DESCRIPTION "RAII-wrappers for unique temporary files and directories that are deleted automatically for C++17 and later. Development package")
+
+include(CPack)
+
