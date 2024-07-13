56432e82c7474ced127ec8d70d230e6676fdfd8c
bugdea1er <bugdealer@icloud.com>
2024-07-13 20:03:00 +0300

    Set IndentWidth to 2

--- src/tmp.cpp
+++ src/tmp.cpp
@@ -40,7 +40,7 @@ constexpr fs::copy_options copy_options =
 /// @returns @c true if a parent directory was newly created, @c false otherwise
 /// @throws std::bad_alloc if memory allocation fails
 bool create_parent(const fs::path& path, std::error_code& ec) {
-    return fs::create_directories(path.parent_path(), ec);
+  return fs::create_directories(path.parent_path(), ec);
 }
 
 /// Creates a temporary path pattern with the given prefix and suffix
@@ -50,24 +50,24 @@ bool create_parent(const fs::path& path, std::error_code& ec) {
 /// @throws std::bad_alloc if memory allocation fails
 fs::path make_pattern(std::string_view prefix, std::string_view suffix) {
 #ifdef WIN32
-    constexpr static std::size_t CHARS_IN_GUID = 39;
-    GUID guid;
-    CoCreateGuid(&guid);
+  constexpr static std::size_t CHARS_IN_GUID = 39;
+  GUID guid;
+  CoCreateGuid(&guid);
 
-    wchar_t name[CHARS_IN_GUID];
-    swprintf(name, CHARS_IN_GUID,
-             L"%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X", guid.Data1,
-             guid.Data2, guid.Data3, guid.Data4[0], guid.Data4[1],
-             guid.Data4[2], guid.Data4[3], guid.Data4[4], guid.Data4[5],
-             guid.Data4[6], guid.Data4[7]);
+  wchar_t name[CHARS_IN_GUID];
+  swprintf(name, CHARS_IN_GUID,
+           L"%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X", guid.Data1,
+           guid.Data2, guid.Data3, guid.Data4[0], guid.Data4[1], guid.Data4[2],
+           guid.Data4[3], guid.Data4[4], guid.Data4[5], guid.Data4[6],
+           guid.Data4[7]);
 #else
-    std::string_view name = "XXXXXX";
+  std::string_view name = "XXXXXX";
 #endif
 
-    fs::path pattern = fs::temp_directory_path() / prefix / name;
+  fs::path pattern = fs::temp_directory_path() / prefix / name;
 
-    pattern += suffix;
-    return pattern;
+  pattern += suffix;
+  return pattern;
 }
 
 /// Creates a temporary file with the given prefix in the system's
@@ -79,40 +79,40 @@ fs::path make_pattern(std::string_view prefix, std::string_view suffix) {
 /// @throws fs::filesystem_error if cannot create the temporary file
 std::pair<fs::path, file::native_handle_type>
 create_file(std::string_view prefix, std::string_view suffix) {
-    fs::path::string_type path = make_pattern(prefix, suffix);
+  fs::path::string_type path = make_pattern(prefix, suffix);
 
-    std::error_code ec;
-    create_parent(path, ec);
-    if (ec) {
-        throw fs::filesystem_error("Cannot create temporary file", ec);
-    }
+  std::error_code ec;
+  create_parent(path, ec);
+  if (ec) {
+    throw fs::filesystem_error("Cannot create temporary file", ec);
+  }
 
 #ifdef WIN32
-    HANDLE handle =
-        CreateFileW(path.c_str(), GENERIC_READ | GENERIC_WRITE,
-                    FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
-                    nullptr, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, nullptr);
+  HANDLE handle =
+      CreateFileW(path.c_str(), GENERIC_READ | GENERIC_WRITE,
+                  FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+                  nullptr, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, nullptr);
 
-    if (handle == INVALID_HANDLE_VALUE) {
-        DWORD err = GetLastError();
-        if (err == ERROR_ALREADY_EXISTS) {
-            return create_file(prefix, suffix);
-        }
-
-        ec = std::error_code(err, std::system_category());
+  if (handle == INVALID_HANDLE_VALUE) {
+    DWORD err = GetLastError();
+    if (err == ERROR_ALREADY_EXISTS) {
+      return create_file(prefix, suffix);
     }
+
+    ec = std::error_code(err, std::system_category());
+  }
 #else
-    int handle = mkstemps(path.data(), static_cast<int>(suffix.size()));
-    if (handle == -1) {
-        ec = std::error_code(errno, std::system_category());
-    }
+  int handle = mkstemps(path.data(), static_cast<int>(suffix.size()));
+  if (handle == -1) {
+    ec = std::error_code(errno, std::system_category());
+  }
 #endif
 
-    if (ec) {
-        throw fs::filesystem_error("Cannot create temporary file", ec);
-    }
+  if (ec) {
+    throw fs::filesystem_error("Cannot create temporary file", ec);
+  }
 
-    return std::pair(path, handle);
+  return std::pair(path, handle);
 }
 
 /// Creates a temporary directory with the given prefix in the system's
@@ -121,34 +121,34 @@ create_file(std::string_view prefix, std::string_view suffix) {
 /// @returns A path to the created temporary directory
 /// @throws fs::filesystem_error if cannot create the temporary directory
 fs::path create_directory(std::string_view prefix) {
-    fs::path::string_type path = make_pattern(prefix, "");
+  fs::path::string_type path = make_pattern(prefix, "");
 
-    std::error_code ec;
-    create_parent(path, ec);
-    if (ec) {
-        throw fs::filesystem_error("Cannot create temporary directory", ec);
-    }
+  std::error_code ec;
+  create_parent(path, ec);
+  if (ec) {
+    throw fs::filesystem_error("Cannot create temporary directory", ec);
+  }
 
 #ifdef WIN32
-    if (!CreateDirectoryW(path.c_str(), nullptr)) {
-        DWORD err = GetLastError();
-        if (err == ERROR_ALREADY_EXISTS) {
-            return create_directory(prefix);
-        }
+  if (!CreateDirectoryW(path.c_str(), nullptr)) {
+    DWORD err = GetLastError();
+    if (err == ERROR_ALREADY_EXISTS) {
+      return create_directory(prefix);
+    }
 
-        ec = std::error_code(err, std::system_category());
-    }
+    ec = std::error_code(err, std::system_category());
+  }
 #else
-    if (mkdtemp(path.data()) == nullptr) {
-        ec = std::error_code(errno, std::system_category());
-    }
+  if (mkdtemp(path.data()) == nullptr) {
+    ec = std::error_code(errno, std::system_category());
+  }
 #endif
 
-    if (ec) {
-        throw fs::filesystem_error("Cannot create temporary directory", ec);
-    }
+  if (ec) {
+    throw fs::filesystem_error("Cannot create temporary directory", ec);
+  }
 
-    return path;
+  return path;
 }
 
 /// Opens a temporary file for writing and returns an output file stream
@@ -157,39 +157,39 @@ fs::path create_directory(std::string_view prefix) {
 /// @param append   Whether to append to the end of the file
 /// @returns An output file stream
 std::ofstream stream(const file& file, bool binary, bool append) noexcept {
-    std::ios::openmode mode = append ? std::ios::app : std::ios::trunc;
-    if (binary) {
-        mode |= std::ios::binary;
-    }
+  std::ios::openmode mode = append ? std::ios::app : std::ios::trunc;
+  if (binary) {
+    mode |= std::ios::binary;
+  }
 
-    return std::ofstream(file.path(), mode);
+  return std::ofstream(file.path(), mode);
 }
 
 /// Deletes the given path recursively, ignoring any errors
 /// @param path     The path to delete
 void remove(const fs::path& path) noexcept {
-    if (!path.empty()) {
-        try {
-            std::error_code ec;
-            fs::remove_all(path, ec);
+  if (!path.empty()) {
+    try {
+      std::error_code ec;
+      fs::remove_all(path, ec);
 
-            fs::path parent = path.parent_path();
-            if (!fs::equivalent(parent, fs::temp_directory_path(), ec)) {
-                fs::remove(parent, ec);
-            }
-        } catch (const std::bad_alloc& ex) {
-            static_cast<void>(ex);
-        }
+      fs::path parent = path.parent_path();
+      if (!fs::equivalent(parent, fs::temp_directory_path(), ec)) {
+        fs::remove(parent, ec);
+      }
+    } catch (const std::bad_alloc& ex) {
+      static_cast<void>(ex);
     }
+  }
 }
 
 /// Closes the given file, ignoring any errors
 /// @param file     The file to close
 void close(const file& file) noexcept {
 #ifdef WIN32
-    CloseHandle(file.native_handle());
+  CloseHandle(file.native_handle());
 #else
-    ::close(file.native_handle());
+  ::close(file.native_handle());
 #endif
 }
 
@@ -199,7 +199,7 @@ void close(const file& file) noexcept {
 /// @param ec   The error code associated with the failure to move the resource
 /// @throws fs::filesystem_error when called
 [[noreturn]] void throw_move_error(const fs::path& to, std::error_code ec) {
-    throw fs::filesystem_error("Cannot move temporary resource", to, ec);
+  throw fs::filesystem_error("Cannot move temporary resource", to, ec);
 }
 }    // namespace
 
@@ -214,54 +214,54 @@ entry::entry(entry&& other) noexcept
     : pathobject(other.release()) {}
 
 entry& entry::operator=(entry&& other) noexcept {
-    remove(*this);
-    pathobject = other.release();
-    return *this;
+  remove(*this);
+  pathobject = other.release();
+  return *this;
 }
 
 entry::~entry() noexcept {
-    remove(*this);
+  remove(*this);
 }
 
 entry::operator const fs::path&() const noexcept {
-    return pathobject;
+  return pathobject;
 }
 
 const fs::path& entry::path() const noexcept {
-    return *this;
+  return *this;
 }
 
 fs::path entry::release() noexcept {
-    fs::path path = std::move(pathobject);
-    pathobject.clear();
+  fs::path path = std::move(pathobject);
+  pathobject.clear();
 
-    return path;
+  return path;
 }
 
 void entry::move(const fs::path& to) {
-    std::error_code ec;
-    create_parent(to, ec);
-    if (ec) {
-        throw_move_error(to, ec);
+  std::error_code ec;
+  create_parent(to, ec);
+  if (ec) {
+    throw_move_error(to, ec);
+  }
+
+  fs::rename(*this, to, ec);
+  if (ec == std::errc::cross_device_link) {
+    if (fs::is_regular_file(*this) && fs::is_directory(to)) {
+      ec = std::make_error_code(std::errc::is_a_directory);
+      throw_move_error(to, ec);
     }
 
-    fs::rename(*this, to, ec);
-    if (ec == std::errc::cross_device_link) {
-        if (fs::is_regular_file(*this) && fs::is_directory(to)) {
-            ec = std::make_error_code(std::errc::is_a_directory);
-            throw_move_error(to, ec);
-        }
+    fs::remove_all(to);
+    fs::copy(*this, to, copy_options, ec);
+  }
 
-        fs::remove_all(to);
-        fs::copy(*this, to, copy_options, ec);
-    }
+  if (ec) {
+    throw_move_error(to, ec);
+  }
 
-    if (ec) {
-        throw_move_error(to, ec);
-    }
-
-    remove(*this);
-    release();
+  remove(*this);
+  release();
 }
 
 //===----------------------------------------------------------------------===//
@@ -280,57 +280,57 @@ file::file(std::string_view prefix, std::string_view suffix)
     : file(prefix, suffix, /*binary=*/true) {}
 
 file file::text(std::string_view prefix, std::string_view suffix) {
-    return file(prefix, suffix, /*binary=*/false);
+  return file(prefix, suffix, /*binary=*/false);
 }
 
 file file::copy(const fs::path& path, std::string_view prefix,
                 std::string_view suffix) {
-    std::error_code ec;
-    file tmpfile = file(prefix, suffix);
+  std::error_code ec;
+  file tmpfile = file(prefix, suffix);
 
-    fs::copy_file(path, tmpfile, copy_options, ec);
+  fs::copy_file(path, tmpfile, copy_options, ec);
 
-    if (ec) {
-        throw fs::filesystem_error("Cannot create a temporary copy", path, ec);
-    }
+  if (ec) {
+    throw fs::filesystem_error("Cannot create a temporary copy", path, ec);
+  }
 
-    return tmpfile;
+  return tmpfile;
 }
 
 file::native_handle_type file::native_handle() const noexcept {
-    return handle;
+  return handle;
 }
 
 std::string file::read() const {
-    std::ios::openmode mode = binary ? std::ios::binary : std::ios::openmode();
-    std::ifstream stream = std::ifstream(path(), mode);
+  std::ios::openmode mode = binary ? std::ios::binary : std::ios::openmode();
+  std::ifstream stream = std::ifstream(path(), mode);
 
-    return std::string(std::istreambuf_iterator<char>(stream), {});
+  return std::string(std::istreambuf_iterator<char>(stream), {});
 }
 
 void file::write(std::string_view content) const {
-    stream(*this, binary, /*append=*/false) << content;
+  stream(*this, binary, /*append=*/false) << content;
 }
 
 void file::append(std::string_view content) const {
-    stream(*this, binary, /*append=*/true) << content;
+  stream(*this, binary, /*append=*/true) << content;
 }
 
 file::~file() noexcept {
-    close(*this);
+  close(*this);
 }
 
 file::file(file&&) noexcept = default;
 
 file& file::operator=(file&& other) noexcept {
-    entry::operator=(std::move(other));
+  entry::operator=(std::move(other));
 
-    close(*this);
+  close(*this);
 
-    this->binary = other.binary;    // NOLINT(bugprone-use-after-move)
-    this->handle = other.handle;    // NOLINT(bugprone-use-after-move)
+  this->binary = other.binary;    // NOLINT(bugprone-use-after-move)
+  this->handle = other.handle;    // NOLINT(bugprone-use-after-move)
 
-    return *this;
+  return *this;
 }
 
 //===----------------------------------------------------------------------===//
@@ -341,24 +341,24 @@ directory::directory(std::string_view prefix)
     : entry(create_directory(prefix)) {}
 
 directory directory::copy(const fs::path& path, std::string_view prefix) {
-    std::error_code ec;
-    directory tmpdir = directory(prefix);
+  std::error_code ec;
+  directory tmpdir = directory(prefix);
 
-    if (fs::is_directory(path)) {
-        fs::copy(path, tmpdir, copy_options, ec);
-    } else {
-        ec = std::make_error_code(std::errc::not_a_directory);
-    }
+  if (fs::is_directory(path)) {
+    fs::copy(path, tmpdir, copy_options, ec);
+  } else {
+    ec = std::make_error_code(std::errc::not_a_directory);
+  }
 
-    if (ec) {
-        throw fs::filesystem_error("Cannot create a temporary copy", path, ec);
-    }
+  if (ec) {
+    throw fs::filesystem_error("Cannot create a temporary copy", path, ec);
+  }
 
-    return tmpdir;
+  return tmpdir;
 }
 
 fs::path directory::operator/(std::string_view source) const {
-    return path() / source;
+  return path() / source;
 }
 
 directory::~directory() noexcept = default;
--- tests/directory.cpp
+++ tests/directory.cpp
@@ -14,137 +14,137 @@ namespace fs = std::filesystem;
 
 /// Tests directory creation with prefix
 TEST(directory, create_with_prefix) {
-    directory tmpdir = directory(PREFIX);
-    fs::path parent = tmpdir.path().parent_path();
+  directory tmpdir = directory(PREFIX);
+  fs::path parent = tmpdir.path().parent_path();
 
-    EXPECT_TRUE(fs::exists(tmpdir));
-    EXPECT_TRUE(fs::is_directory(tmpdir));
-    EXPECT_TRUE(fs::equivalent(parent, fs::temp_directory_path() / PREFIX));
+  EXPECT_TRUE(fs::exists(tmpdir));
+  EXPECT_TRUE(fs::is_directory(tmpdir));
+  EXPECT_TRUE(fs::equivalent(parent, fs::temp_directory_path() / PREFIX));
 
-    fs::perms permissions = fs::status(tmpdir).permissions();
+  fs::perms permissions = fs::status(tmpdir).permissions();
 #ifdef WIN32
-    // Following the logic with GetTempFileNameW
-    EXPECT_EQ(permissions, fs::perms::all);
+  // Following the logic with GetTempFileNameW
+  EXPECT_EQ(permissions, fs::perms::all);
 #else
-    // mkdtemp creates a directory with full access only for the owner
-    EXPECT_EQ(permissions, fs::perms::owner_all);
+  // mkdtemp creates a directory with full access only for the owner
+  EXPECT_EQ(permissions, fs::perms::owner_all);
 #endif
 }
 
 /// Tests directory creation without prefix
 TEST(directory, create_without_prefix) {
-    directory tmpdir = directory();
-    fs::path parent = tmpdir.path().parent_path();
+  directory tmpdir = directory();
+  fs::path parent = tmpdir.path().parent_path();
 
-    EXPECT_TRUE(fs::exists(tmpdir));
-    EXPECT_TRUE(fs::is_directory(tmpdir));
-    EXPECT_TRUE(fs::equivalent(parent, fs::temp_directory_path()));
+  EXPECT_TRUE(fs::exists(tmpdir));
+  EXPECT_TRUE(fs::is_directory(tmpdir));
+  EXPECT_TRUE(fs::equivalent(parent, fs::temp_directory_path()));
 }
 
 /// Tests multiple directories creation with the same prefix
 TEST(directory, create_multiple) {
-    directory fst = directory(PREFIX);
-    directory snd = directory(PREFIX);
+  directory fst = directory(PREFIX);
+  directory snd = directory(PREFIX);
 
-    EXPECT_FALSE(fs::equivalent(fst, snd));
+  EXPECT_FALSE(fs::equivalent(fst, snd));
 }
 
 /// Tests creation of a temporary copy of a directory
 TEST(directory, copy_directory) {
-    directory tmpdir = directory(PREFIX);
-    std::ofstream(tmpdir / "file") << "Hello, world!";
+  directory tmpdir = directory(PREFIX);
+  std::ofstream(tmpdir / "file") << "Hello, world!";
 
-    directory tmpcopy = directory::copy(tmpdir, PREFIX);
-    EXPECT_TRUE(fs::exists(tmpdir));
-    EXPECT_TRUE(fs::exists(tmpcopy));
-    EXPECT_FALSE(fs::equivalent(tmpdir, tmpcopy));
+  directory tmpcopy = directory::copy(tmpdir, PREFIX);
+  EXPECT_TRUE(fs::exists(tmpdir));
+  EXPECT_TRUE(fs::exists(tmpcopy));
+  EXPECT_FALSE(fs::equivalent(tmpdir, tmpcopy));
 
-    EXPECT_TRUE(fs::is_directory(tmpcopy));
+  EXPECT_TRUE(fs::is_directory(tmpcopy));
 
-    std::ifstream stream = std::ifstream(tmpcopy / "file");
-    auto content = std::string(std::istreambuf_iterator<char>(stream), {});
-    EXPECT_EQ(content, "Hello, world!");
+  std::ifstream stream = std::ifstream(tmpcopy / "file");
+  auto content = std::string(std::istreambuf_iterator<char>(stream), {});
+  EXPECT_EQ(content, "Hello, world!");
 }
 
 /// Tests creation of a temporary copy of a file
 TEST(directory, copy_file) {
-    file tmpfile = file(PREFIX);
-    EXPECT_THROW(directory::copy(tmpfile, PREFIX), fs::filesystem_error);
+  file tmpfile = file(PREFIX);
+  EXPECT_THROW(directory::copy(tmpfile, PREFIX), fs::filesystem_error);
 }
 
 /// Tests `operator/` of directory
 TEST(directory, subpath) {
-    directory tmpdir = directory(PREFIX);
-    fs::path child = tmpdir / "child";
+  directory tmpdir = directory(PREFIX);
+  fs::path child = tmpdir / "child";
 
-    EXPECT_TRUE(fs::equivalent(tmpdir, child.parent_path()));
+  EXPECT_TRUE(fs::equivalent(tmpdir, child.parent_path()));
 }
 
 /// Tests that destructor removes a directory
 TEST(directory, destructor) {
-    fs::path path = fs::path();
-    {
-        directory tmpdir = directory(PREFIX);
-        path = tmpdir;
-    }
+  fs::path path = fs::path();
+  {
+    directory tmpdir = directory(PREFIX);
+    path = tmpdir;
+  }
 
-    EXPECT_FALSE(fs::exists(path));
+  EXPECT_FALSE(fs::exists(path));
 }
 
 /// Tests directory move constructor
 TEST(directory, move_constructor) {
-    directory fst = directory(PREFIX);
-    directory snd = std::move(fst);
+  directory fst = directory(PREFIX);
+  directory snd = std::move(fst);
 
-    EXPECT_TRUE(fst.path().empty());
-    EXPECT_TRUE(fs::exists(snd));
+  EXPECT_TRUE(fst.path().empty());
+  EXPECT_TRUE(fs::exists(snd));
 }
 
 /// Tests directory move assignment operator
 TEST(directory, move_assignment) {
-    directory fst = directory(PREFIX);
-    directory snd = directory(PREFIX);
+  directory fst = directory(PREFIX);
+  directory snd = directory(PREFIX);
 
-    fs::path path1 = fst;
-    fs::path path2 = snd;
+  fs::path path1 = fst;
+  fs::path path2 = snd;
 
-    fst = std::move(snd);
+  fst = std::move(snd);
 
-    EXPECT_FALSE(fs::exists(path1));
-    EXPECT_TRUE(fs::exists(path2));
+  EXPECT_FALSE(fs::exists(path1));
+  EXPECT_TRUE(fs::exists(path2));
 
-    EXPECT_TRUE(fs::exists(fst));
-    EXPECT_TRUE(fs::equivalent(fst, path2));
+  EXPECT_TRUE(fs::exists(fst));
+  EXPECT_TRUE(fs::equivalent(fst, path2));
 }
 
 /// Tests directory releasing
 TEST(directory, release) {
-    fs::path path = fs::path();
-    {
-        directory tmpdir = directory(PREFIX);
-        fs::path expected = tmpdir;
-        path = tmpdir.release();
+  fs::path path = fs::path();
+  {
+    directory tmpdir = directory(PREFIX);
+    fs::path expected = tmpdir;
+    path = tmpdir.release();
 
-        EXPECT_TRUE(fs::equivalent(path, expected));
-    }
+    EXPECT_TRUE(fs::equivalent(path, expected));
+  }
 
-    EXPECT_TRUE(fs::exists(path));
-    fs::remove(path);
+  EXPECT_TRUE(fs::exists(path));
+  fs::remove(path);
 }
 
 /// Tests directory moving
 TEST(directory, move) {
-    fs::path path = fs::path();
-    fs::path to = fs::temp_directory_path() / PREFIX / "non-existing/parent";
-    {
-        directory tmpdir = directory(PREFIX);
-        path = tmpdir;
+  fs::path path = fs::path();
+  fs::path to = fs::temp_directory_path() / PREFIX / "non-existing/parent";
+  {
+    directory tmpdir = directory(PREFIX);
+    path = tmpdir;
 
-        tmpdir.move(to);
-    }
+    tmpdir.move(to);
+  }
 
-    EXPECT_FALSE(fs::exists(path));
-    EXPECT_TRUE(fs::exists(to));
-    fs::remove_all(fs::temp_directory_path() / PREFIX / "non-existing");
+  EXPECT_FALSE(fs::exists(path));
+  EXPECT_TRUE(fs::exists(to));
+  fs::remove_all(fs::temp_directory_path() / PREFIX / "non-existing");
 }
 }    // namespace tmp
