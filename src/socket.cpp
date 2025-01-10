37575b1393437d3db5225306b6dc4f2dfadd7c8d
bugdea1er <bugdealer@icloud.com>
2025-01-10 22:10:28 +0300

    Implement `tmp::socket`

---
+++ src/socket.cpp
@@ -0,0 +1,127 @@
+#include <tmp/entry>
+#include <tmp/socket>
+
+#include "utils.hpp"
+
+#include <cstddef>
+#include <filesystem>
+#include <string_view>
+#include <system_error>
+#include <utility>
+
+#ifndef _WIN32
+#include <cerrno>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#endif
+
+namespace tmp {
+namespace {
+
+/// Creates a temporary unix domain socket at the given path
+/// @note The given path will be overwritten
+/// @param path     A path for the temporary socket
+/// @returns A path to the created temporary socket and a handle to it
+/// @throws std::filesystem::filesystem_error if cannot create a socket
+std::pair<fs::path, entry::native_handle_type> create_socket(fs::path path) {
+  if (fs::exists(path)) {
+    fs::remove_all(path);
+  }
+
+  // FIXME: check if the filename isn't too long
+
+  sockaddr_un address;
+  address.sun_len = sizeof(address);
+  address.sun_family = AF_UNIX;
+  memcpy(address.sun_path, path.c_str(), path.string().length() + 1);
+
+  entry::native_handle_type handle = ::socket(PF_LOCAL, SOCK_STREAM, 0);
+  // FIXME: check if handle is valid
+
+  int ret = bind(handle, (sockaddr*)&address, sizeof(address));
+  if (ret != 0) {
+    std::error_code ec = std::error_code(errno, std::system_category());
+    throw fs::filesystem_error("Cannot create a temporary socket", ec);
+  }
+
+  listen(handle, SOMAXCONN);    // FIXME: check for errors
+  return std::pair(std::move(path), handle);
+}
+
+/// Creates a temporary unix domain socket with the given prefix in the system's
+/// temporary directory
+/// @param label     A label to attach to the temporary socket path
+/// @returns A path to the created temporary socket and a handle to it
+/// @throws fs::filesystem_error  if cannot create a temporary socket
+/// @throws std::invalid_argument if the label is ill-formatted
+std::pair<fs::path, entry::native_handle_type>
+create_socket(std::string_view label) {
+  fs::path::string_type path = make_pattern(label, "");
+
+  std::error_code ec;
+  create_parent(path, ec);
+  if (ec) {
+    throw fs::filesystem_error("Cannot create a temporary socket", ec);
+  }
+
+  mktemp(path.data());    // FIXME: check for errors
+  return create_socket(fs::path(path));
+}
+}    // namespace
+
+socket::socket(std::string_view label)
+    : entry(create_socket(label)) {}
+
+socket::socket(fs::path path)
+    : entry(create_socket(std::move(path))) {}
+
+std::future<void> socket::listen(acceptor function) {
+  return std::async(std::launch::async, [&, function] {
+    while (true) {
+      int client = accept(native_handle(), nullptr, nullptr);
+      if (client < 0) {
+        if (errno == EBADF) {    // closed via `socket::shutdown()`
+          break;
+        }
+
+        continue;
+      }
+
+      std::string input;
+      input.resize(1024);    // FIXME: magic constant
+
+      ssize_t received = recv(client, input.data(), input.capacity(), 0);
+      if (received <= 0) {
+        close(client);
+        continue;
+      }
+
+      input.resize(received - 1);    // FIXME: strange
+
+      std::string response = function(input);
+
+      send(client, response.c_str(), response.size(), 0);
+      close(client);
+    }
+  });
+}
+
+void socket::shutdown() {
+  int ret = ::close(native_handle());    // FIXME: shutdown(fd, SHUT_RD)?
+  if (ret != 0) {
+    std::error_code ec = std::error_code(errno, std::system_category());
+    throw fs::filesystem_error("Cannot shutdown a temporary socket", ec);
+  }
+}
+
+socket::~socket() noexcept = default;
+
+socket::socket(socket&&) noexcept = default;
+socket& socket::operator=(socket&& other) noexcept = default;
+}    // namespace tmp
+
+std::size_t
+std::hash<tmp::socket>::operator()(const tmp::socket& socket) const noexcept {
+  return std::hash<tmp::entry>()(socket);
+}
---
+++ tests/socket.cpp
@@ -0,0 +1,172 @@
+#include <tmp/socket>
+
+#include "utils.hpp"
+
+#include <gtest/gtest.h>
+
+#include <sys/socket.h>
+#include <sys/un.h>
+
+namespace tmp {
+namespace {
+
+namespace fs = std::filesystem;
+
+std::string acceptor(std::string_view input) noexcept {
+  EXPECT_EQ(input, "Hello, world!");
+  return "Bye, world!";
+}
+
+/// Tests socket creation with label
+TEST(socket, create_with_label) {
+  socket tmpsocket = socket(std::string_view(LABEL));
+  fs::path parent = tmpsocket.path().parent_path();
+
+  EXPECT_TRUE(fs::exists(tmpsocket));
+  EXPECT_TRUE(fs::is_socket(tmpsocket));
+  EXPECT_TRUE(fs::equivalent(parent, fs::temp_directory_path() / LABEL));
+  EXPECT_TRUE(native_handle_is_valid(tmpsocket.native_handle()));
+
+  // FIXME: what should it be?
+  // fs::perms permissions = fs::status(tmpsocket).permissions();
+  // EXPECT_EQ(permissions, fs::perms::owner_read | fs::perms::owner_write);
+}
+
+/// Tests socket creation without label
+TEST(socket, create_without_label) {
+  socket tmpsocket = socket();
+  fs::path parent = tmpsocket.path().parent_path();
+
+  EXPECT_TRUE(fs::exists(tmpsocket));
+  EXPECT_TRUE(fs::is_socket(tmpsocket));
+  EXPECT_TRUE(fs::equivalent(parent, fs::temp_directory_path()));
+  EXPECT_TRUE(native_handle_is_valid(tmpsocket.native_handle()));
+}
+
+/// Tests socket creation with path
+TEST(socket, create_with_path) {
+  socket tmpsocket = socket(fs::current_path() / "socket");
+  fs::path parent = tmpsocket.path().parent_path();
+
+  EXPECT_TRUE(fs::exists(tmpsocket));
+  EXPECT_TRUE(fs::is_socket(tmpsocket));
+  EXPECT_TRUE(fs::equivalent(parent, fs::current_path()));
+  EXPECT_TRUE(native_handle_is_valid(tmpsocket.native_handle()));
+}
+
+/// Tests socket listening
+TEST(socket, listening) {
+  socket tmpsocket = socket();
+
+  auto _ = tmpsocket.listen(&acceptor);
+
+  sockaddr_un address;
+  address.sun_len = sizeof(address);
+  address.sun_family = AF_UNIX;
+  memcpy(address.sun_path, tmpsocket.path().c_str(),
+         tmpsocket.path().string().length() + 1);
+
+  int client = ::socket(PF_LOCAL, SOCK_STREAM, 0);
+  connect(client, (sockaddr*)&address, sizeof(address));
+
+  char buffer[1024];
+  int read_count;
+
+  strcpy(buffer, "Hello, world!");
+  write(client, buffer, strlen(buffer) + 1);
+  read_count = read(client, buffer, 1024);
+
+  EXPECT_EQ(std::string_view(buffer, read_count), "Bye, world!");
+  EXPECT_NO_THROW(tmpsocket.shutdown());
+  std::cout << "Closing socket" << std::endl;
+}
+
+/// Tests that destructor removes a socket
+TEST(socket, destructor) {
+  fs::path path;
+  entry::native_handle_type handle;
+  {
+    socket tmpsocket = socket();
+    path = tmpsocket;
+    handle = tmpsocket.native_handle();
+  }
+
+  EXPECT_FALSE(fs::exists(path));
+  EXPECT_FALSE(native_handle_is_valid(handle));
+}
+
+/// Tests socket move constructor
+TEST(socket, move_constructor) {
+  socket fst = socket();
+  socket snd = socket(std::move(fst));
+
+  fst.~socket();    // NOLINT(*-use-after-move)
+
+  EXPECT_FALSE(snd.path().empty());
+  EXPECT_TRUE(fs::exists(snd));
+  EXPECT_TRUE(native_handle_is_valid(snd.native_handle()));
+}
+
+/// Tests socket move assignment operator
+TEST(socket, move_assignment) {
+  socket fst = socket();
+  {
+    socket snd = socket();
+
+    fs::path path1 = fst;
+    fs::path path2 = snd;
+
+    entry::native_handle_type fst_handle = fst.native_handle();
+    entry::native_handle_type snd_handle = snd.native_handle();
+
+    fst = std::move(snd);
+
+    EXPECT_FALSE(fs::exists(path1));
+    EXPECT_TRUE(fs::exists(path2));
+
+    EXPECT_TRUE(fs::exists(fst));
+    EXPECT_TRUE(fs::equivalent(fst, path2));
+
+    EXPECT_FALSE(native_handle_is_valid(fst_handle));
+    EXPECT_TRUE(native_handle_is_valid(snd_handle));
+  }
+
+  EXPECT_FALSE(fst.path().empty());
+  EXPECT_TRUE(native_handle_is_valid(fst.native_handle()));
+}
+
+/// Tests socket swapping
+TEST(socket, swap) {
+  socket fst = socket();
+  socket snd = socket();
+
+  fs::path fst_path = fst.path();
+  fs::path snd_path = snd.path();
+  entry::native_handle_type fst_handle = fst.native_handle();
+  entry::native_handle_type snd_handle = snd.native_handle();
+
+  std::swap(fst, snd);
+
+  EXPECT_EQ(fst.path(), snd_path);
+  EXPECT_EQ(snd.path(), fst_path);
+  EXPECT_EQ(fst.native_handle(), snd_handle);
+  EXPECT_EQ(snd.native_handle(), fst_handle);
+}
+
+/// Tests socket hashing
+TEST(socket, hash) {
+  socket tmpsocket = socket();
+  std::hash hash = std::hash<socket>();
+
+  EXPECT_EQ(hash(tmpsocket), fs::hash_value(tmpsocket.path()));
+}
+
+/// Tests socket relational operators
+TEST(socket, relational) {
+  socket tmpsocket = socket();
+
+  EXPECT_TRUE(tmpsocket == tmpsocket);
+  EXPECT_FALSE(tmpsocket < tmpsocket);
+}
+}    // namespace
+}    // namespace tmp
