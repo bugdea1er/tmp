4631ffae9f87ae555b8b97de15915b18b50384f2
Ilya Andreev <bugdealer@icloud.com>
2025-01-25 02:41:43 +0300

    Make `create` accept `openmode` as a parameter (#154)
    Follow up pull requests will accept `openmode` in `file` constructors

--- src/create.hpp
+++ src/create.hpp
@@ -3,6 +3,7 @@
 
 #include <filesystem>
 #include <fstream>
+#include <ios>
 #include <string_view>
 #include <system_error>
 #include <utility>
@@ -14,23 +15,25 @@ namespace fs = std::filesystem;
 /// temporary directory, and opens it for reading and writing
 /// @param[in] label     A label to attach to the temporary file path
 /// @param[in] extension An extension of the temporary file path
-/// @param[in] binary    Whether to open the file in binary mode
+/// @param[in] mode      Specifies stream open mode
 /// @returns A path to the created temporary file and a handle to it
 /// @throws fs::filesystem_error  if cannot create a temporary file
 /// @throws std::invalid_argument if the label or extension is ill-formatted
-std::pair<fs::path, std::filebuf>
-create_file(std::string_view label, std::string_view extension, bool binary);
+std::pair<fs::path, std::filebuf> create_file(std::string_view label,
+                                              std::string_view extension,
+                                              std::ios::openmode mode);
 
 /// Creates a temporary file with the given label and extension in the system's
 /// temporary directory, and opens it for reading and writing
 /// @param[in]  label     A label to attach to the temporary file path
 /// @param[in]  extension An extension of the temporary file path
-/// @param[in]  binary    Whether to open the file in binary mode
+/// @param[in]  mode      Specifies stream open mode
 /// @param[out] ec        Parameter for error reporting
 /// @returns A path to the created temporary file and a handle to it
 std::pair<fs::path, std::filebuf> create_file(std::string_view label,
                                               std::string_view extension,
-                                              bool binary, std::error_code& ec);
+                                              std::ios::openmode mode,
+                                              std::error_code& ec);
 
 /// Creates a temporary directory with the given label in the system's
 /// temporary directory
