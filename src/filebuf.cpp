49bf0e86f8b31a7d7f99a05afc00782d5f2b13da
bugdea1er <bugdealer@icloud.com>
2025-02-12 14:25:49 +0300

    Tidy up filebuf documentation

--- src/filebuf.cpp
+++ src/filebuf.cpp
@@ -3,8 +3,8 @@
 #include <ios>
 
 #ifdef _WIN32
-#include <cstdio>
 #include <corecrt_io.h>
+#include <cstdio>
 #endif
 
 #if __has_include(<__config>)
@@ -12,20 +12,12 @@
 #endif
 
 namespace tmp {
-namespace {
-}
 
-std::filebuf* filebuf::open(native_handle_type handle, std::ios::openmode mode) {
-#if defined(_LIBCPP_VERSION) && _LIBCPP_VERSION >= 70000
-  // LLVM libc++ supports filesystem library since version 7
-  // Apple LLVM, despite having a different version scheme still supplies original _LIBCPP_VERSION
-  // Apple Clang supports filesystem library since version 11.0.0, and requires macOS 10.15 or newer (TODO: check other apple platforms?)
-  return this->__open(handle, mode);
-#elif defined(__GLIBCXX__)    // TODO: check versions
-  // GCC libstdc++ supports filesystem library since 8
-  // https://en.cppreference.com/w/cpp/compiler_support/17#C.2B.2B17_library_features
-
-  // On libstdc++ we replicate a standard `std::basic_filebuf::open`
+filebuf* filebuf::open(native_handle_type handle, std::ios::openmode mode) {
+#if defined(_LIBCPP_VERSION)
+  return this->__open(handle, mode) != nullptr ? this : nullptr;
+#elif defined(__GLIBCXX__)
+  // We replicate the standard `std::basic_filebuf::open`
   this->_M_file.sys_open(handle, mode);
   if (this->is_open()) {
     this->_M_allocate_internal_buffer();
@@ -42,17 +34,15 @@ std::filebuf* filebuf::open(native_handle_type handle, std::ios::openmode mode) 
   }
 
   return nullptr;
-#endif
-
-#if defined(_MSC_VER) && _MSC_VER >= 1914
+#elif defined(_MSC_VER)
   if (handle == nullptr) {
     return nullptr;
   }
 
   _Init(handle, _Newfl);
   return this;
+#elif
+#error "Target C++ standard library is not supported"
 #endif
-
-  // Other C++ STL implementations may be supported if needed and possible
 }
-}
+}    // namespace tmp
