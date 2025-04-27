67d8cf23493ead7b2bf69f908010505ab3308f2d
Ilya Andreev <bugdealer@icloud.com>
2025-04-27 23:48:41 +0300

    Always set `__declspec(dllexport)` for classes (#203)
    Apparently you can always specify `dllexport` for library classes on
    Windows, so this pull request eliminates the need to add the
    `TMP_BUILDING_DLL` compilation definition

--- src/abi.hpp
+++ src/abi.hpp
@@ -1,17 +1,14 @@
 #ifndef TMP_ABI_H
 #define TMP_ABI_H
 
-#if defined _WIN32
-#if defined TMP_BUILDING_DLL
+#ifdef _WIN32
 #define abi __declspec(dllexport)
-#else
-#define abi
-#endif
 #else
 #define abi __attribute__((visibility("default")))
 #endif
 
 namespace tmp {
+
 class abi directory;
 class abi file;
 }    // namespace tmp
