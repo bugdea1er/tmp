c08a319660844d5ead6183028d21f980d8a0f0da
bugdea1er <bugdealer@icloud.com>
2025-04-27 23:47:33 +0300

    Use `ifdef` for `abi` macro definition

--- src/abi.hpp
+++ src/abi.hpp
@@ -1,13 +1,14 @@
 #ifndef TMP_ABI_H
 #define TMP_ABI_H
 
-#if defined _WIN32
+#ifdef _WIN32
 #define abi __declspec(dllexport)
 #else
 #define abi __attribute__((visibility("default")))
 #endif
 
 namespace tmp {
+
 class abi directory;
 class abi file;
 }    // namespace tmp
