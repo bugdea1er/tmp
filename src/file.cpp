720157bbab6e2e5a83e0a188cedd645cbf2ae1f8
Ilya Andreev <bugdealer@icloud.com>
2025-06-21 21:11:26 +0300

    Breaking: Remove `openmode` parameter from file construction (#218)
    According to C99 Rationale, unnamed temporary files are expected to be written first and then read as transparently as possible, so they are created in binary update mode
    
    > ### 7.19.4.3 The tmpfile function
    > The tmpfile function is intended to allow users to create binary “scratch” files. The as if principle implies that the information in such a file need never actually be stored on a file-structured device.
    > The temporary file is created in binary update mode because it will presumably be first written and then read as transparently as possible. Trailing null-character padding may cause problems for some existing programs.
    
    https://www.open-std.org/jtc1/sc22/wg14/www/C99RationaleV5.10.pdf
    
    I don't see any point in implementing `tmp::file` in any other way, given that we haven't found a practical use for text or append modes in production for over a year now

--- src/file.cpp
+++ src/file.cpp
@@ -25,6 +25,11 @@ namespace {
 
 namespace fs = std::filesystem;
 
+#ifndef _MSC_VER
+/// Open mode for binary temporary files
+constexpr auto mode = std::ios::binary | std::ios::in | std::ios::out;
+#endif
+
 /// Returns an implementation-defined handle to the file
 /// @param[in] file The file to the native handle for
 /// @returns The underlying implementation-defined handle
@@ -36,106 +41,13 @@ file::native_handle_type get_native_handle(std::FILE* file) noexcept {
 #endif
 }
 
-/// Makes a mode string for opening a temporary file
-/// @param[in] mode The file opening mode
-/// @returns A suitable mode string
-const char* make_mdstring(std::ios::openmode mode) noexcept {
-  // Special case: the C++ standard forbids `app` and `trunc` at the same time
-  if ((mode & std::ios::app) != 0 && (mode & std::ios::trunc) != 0) {
-    return nullptr;
-  }
-
-  // - `std::ios::in` and `std::ios::out` are always applied
-  // - `std::ios::trunc` has no effect on the empty file
-  // - `std::ios::noreplace` has no effect for temporary files
-  // - any other platform dependent flag is not supported
-  unsigned filtered = mode & (std::ios::app | std::ios::binary);
-
-  switch (filtered) {
-  case 0:
-#ifdef _WIN32
-    return "w+TD";
-#else
-    return "w+";
-#endif
-  case std::ios::app:
-#ifdef _WIN32
-    return "a+TD";
-#else
-    return "a+";
-#endif
-  case std::ios::binary:
-#ifdef _WIN32
-    return "w+bTD";
-#else
-    return "w+b";
-#endif
-  case std::ios::app | std::ios::binary:
-#ifdef _WIN32
-    return "a+bTD";
-#else
-    return "a+b";
-#endif
-  default:
-    return nullptr;
-  }
-}
-
-/// Reopens the given temporary file with the given open mode
-/// @param[in]  mdstring The temporary file opening mode
-/// @param[out] file     The file to reopen
-/// @param[out] ec       Parameter for error reporting
-void reopen_file(const char* mdstring, std::FILE* file,
-                 std::error_code& ec) noexcept {
-  ec.clear();
-
-#ifdef _WIN32
-  HANDLE handle = get_native_handle(file);
-
-  std::string path;
-  path.resize(MAX_PATH);
-  DWORD ret = GetFinalPathNameByHandleA(handle, path.data(), MAX_PATH, 0);
-  if (ret == 0) {
-    ec.assign(GetLastError(), std::system_category());
-    return;
-  }
-
-  path.resize(ret);
-
-  // FIXME: freopen loses shared access rules
-  file = freopen(path.c_str(), mdstring, file);
-  if (file == nullptr) {
-    ec.assign(errno, std::generic_category());
-  }
-#else
-  file = std::freopen(nullptr, mdstring, file);
-  if (file == nullptr) {
-    ec.assign(errno, std::generic_category());
-  }
-#endif
-}
-
-/// Creates and opens a temporary file in the current user's temporary directory
-/// @param[in] mode The file opening mode
-/// @returns A handle to the created temporary file
+/// Creates and opens a binary temporary file as if by POSIX `tmpfile`
+/// @returns A pointer to the file stream associated with the temporary file
 /// @throws fs::filesystem_error if cannot create a temporary file
-/// @throws std::invalid_argument if the given openmode is invalid
-std::FILE* create_file(std::ios::openmode mode) {
-  const char* mdstring = make_mdstring(mode);
-  if (mdstring == nullptr) {
-    throw std::invalid_argument(
-        "Cannot create a temporary file: invalid openmode");
-  }
-
-  std::error_code ec;
+std::FILE* create_file() {
   std::FILE* file = std::tmpfile();
   if (file == nullptr) {
-    ec.assign(errno, std::generic_category());
-  } else {
-    reopen_file(mdstring, file, ec);
-  }
-
-  if (ec) {
+    std::error_code ec = std::error_code(errno, std::generic_category());
     throw fs::filesystem_error("Cannot create a temporary file", ec);
   }
 
@@ -143,11 +55,9 @@ std::FILE* create_file(std::ios::openmode mode) {
 }
 }    // namespace
 
-file::file(std::ios::openmode mode)
+file::file()
     : std::iostream(std::addressof(sb)),
-      underlying(create_file(mode), &std::fclose) {
-  mode |= std::ios::in | std::ios::out;
-
+      underlying(create_file(), &std::fclose) {
 #if defined(_MSC_VER)
   sb = std::filebuf(underlying.get());
 #elif defined(_LIBCPP_VERSION)
@@ -157,8 +67,8 @@ file::file(std::ios::openmode mode)
 #endif
 
   if (!sb.is_open()) {
-    throw std::invalid_argument(
-        "Cannot create a temporary file: invalid openmode");
+    std::error_code ec = std::make_error_code(std::io_errc::stream);
+    throw fs::filesystem_error("Cannot create a temporary file", ec);
   }
 }
 
