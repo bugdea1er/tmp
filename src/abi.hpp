ac00ef0f8517da3c92cf5a974c43225c9ee3f40d
bugdea1er <bugdealer@icloud.com>
2025-08-31 14:07:54 +0300

    Fix GCC build

--- src/abi.hpp
+++ src/abi.hpp
@@ -16,15 +16,15 @@
 
 namespace tmp {
 
-std::filesystem::path abi create_directory(std::string_view prefix);
-void abi remove_all(const std::filesystem::path& path) noexcept;
+auto abi create_directory(std::string_view prefix) -> std::filesystem::path;
+auto abi remove_all(const std::filesystem::path& path) noexcept -> void;
 
-std::FILE* abi create_file();
-
+auto abi create_file() -> std::FILE*;
+auto abi get_native_handle(std::FILE* file) noexcept ->
 #if defined(_WIN32)
-void* abi get_native_handle(std::FILE* file) noexcept;
+    void*;
 #elif __has_include(<unistd.h>)
-int abi get_native_handle(std::FILE* file) noexcept;
+    int;
 #endif
 }    // namespace tmp
 
