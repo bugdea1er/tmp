7d24010b5a5f68e26195bb3d382fa41e839b9dff
bugdea1er <bugdealer@icloud.com>
2025-08-31 12:34:35 +0300

    Make all `file` methods inline

--- src/abi.hpp
+++ src/abi.hpp
@@ -4,6 +4,7 @@
 #ifndef TMP_ABI_H
 #define TMP_ABI_H
 
+#include <cstdio>
 #include <filesystem>
 #include <string_view>
 
@@ -18,7 +19,13 @@ namespace tmp {
 std::filesystem::path abi create_directory(std::string_view prefix);
 void abi remove_all(const std::filesystem::path& path) noexcept;
 
-class abi file;
+std::FILE* abi create_file();
+
+#if defined(_WIN32)
+void* abi get_native_handle(std::FILE* file) noexcept;
+#elif __has_include(<unistd.h>)
+int abi get_native_handle(std::FILE* file) noexcept;
+#endif
 }    // namespace tmp
 
 #endif    // TMP_ABI_H
